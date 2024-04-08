3ef3c02116840195953fc4993addb14b989e3340
skjalosc <skjalosc@arcada.fi>
2024-04-08 11:01:12 +0300

    Nyaste 4.4.2024

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,9 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,58 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    EditText inputText;
+
+    double[] values = { 3.5,4.4,9.0,1.2,6.5,12.0 };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        outputText = findViewById(R.id.outText);
+        inputText = findViewById(R.id.inputText);
+
+        double version = 3.14159;
+        int year = 2024;
+        String author = "Oscar";
+
+        outputText.setText(String.format(
+                "Appens version %.1f år %d by %s"
+                , version
+                , year
+                , author));
+
+        outputText.setText("Appen funkar");
     }
-}
+
+
+
+    public void calculate(View view) {
+
+        double[] values = { 3.5,4.4,9.0,1.2,6.5,12.0 };
+
+        double sum = 0;
+
+        for (int i = 0; i <values.length; i++) {
+
+            sum += values[i];
+
+
+            double mean = sum / values.length;
+
+            outputText.setText(String.format("medelvärde %.2f" , Statistics.calcmean(values)));
+
+
+        }
+
+    }
+        public void buttonHandler(View view) {
+                outputText.setText(inputText.getText().toString());
+
+
+        }
+
+    }
---
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -0,0 +1,19 @@
+package fi.arcada.codechallenge;
+
+public class Statistics {
+
+
+    public static double calcmean(double[] values) {
+        double sum = 0;
+
+        for (int i = 0; i < values.length; i++) {
+
+            sum += values[i];
+        }
+
+            return sum / values.length;
+
+        }
+
+
+    }
